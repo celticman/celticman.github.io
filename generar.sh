6053e0e95c428516993e7d96089cb46f3d6648be
Celticman <celtiman@celticman.io>
2022-08-31 17:55:44 +0200

    Cambios en pie usando variables

--- generar.sh
+++ generar.sh
@@ -1,5 +1,5 @@
 rm *.html
 DIRECTORIO=$(pwd)
 for fichero in *.md; do 
-    pandoc --standalone -t html5 --data-dir=$DIRECTORIO --template "celticman_plantilla.html5" "$fichero" -o "${fichero%.md}.html" 
+    pandoc --standalone -t html5 --data-dir=$DIRECTORIO --template "celticman_plantilla.html5" "$fichero" -o "${fichero%.md}.html" -V pandocversion="$(pandoc --version | head -n 1)" -V curdate="$(date -u --iso-8601)" 
 done
